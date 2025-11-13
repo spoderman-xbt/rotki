70c0a96926f039d130003072e43a38842a8c5982
spoderman-xbt <233967958+spoderman-xbt@users.noreply.github.com>
2025-11-13 13:04:14 +0100

    LGTM

--- rotkehlchen/tests/api/test_settings.py
+++ rotkehlchen/tests/api/test_settings.py
@@ -393,6 +393,16 @@ def test_custom_bitcoin_api(rotkehlchen_api_server: 'APIServer') -> None:
     assert json_data['message'] == ''
     assert result
 
+    # Get the current list again to ensure it's actually deleted
+    response = requests.get(
+        api_url_for(rotkehlchen_api_server, 'rpcnodesresource', blockchain='btc'),
+    )
+    assert_proper_response(response)
+    json_data = response.json()
+    assert json_data['message'] == ''
+    result = json_data['result']
+    assert result == []
+
 
 def test_disable_taxfree_after_period(rotkehlchen_api_server: 'APIServer') -> None:
     """Test that providing -1 for the taxfree_after_period setting disables it """
