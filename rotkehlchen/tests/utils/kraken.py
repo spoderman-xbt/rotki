0b57f0d26a33304fc9714ae0874e50c443f6a494
spoderman-xbt <233967958+spoderman-xbt@users.noreply.github.com>
2025-11-22 22:04:20 +0100

    Getting error on Kraken test querying balances but not sure why

--- rotkehlchen/tests/utils/kraken.py
+++ rotkehlchen/tests/utils/kraken.py
@@ -3,7 +3,6 @@ import random
 from pathlib import Path
 from typing import Any
 
-from rotkehlchen.constants import KRAKEN_BASE_URL
 from rotkehlchen.db.dbhandler import DBHandler
 from rotkehlchen.errors.misc import RemoteError
 from rotkehlchen.exchanges.kraken.kraken import Kraken
@@ -527,20 +526,20 @@ class MockKraken(Kraken):
 
         self.balance_data_return = {'XXBT': '5.0', 'XETH': '10.0', 'NOTAREALASSET': '15.0'}
         # Not required in the real Kraken instance but we use it in the tests
-        self.tradeable_pairs = self.api_query(KRAKEN_BASE_URL, 'AssetPairs')
+        self.tradeable_pairs = self.api_query('AssetPairs')
 
     @staticmethod
     def _load_results_from_file(filename: str) -> dict[str, Any]:
         dir_path = Path(__file__).resolve().parent.parent
         return jsonloads_dict((dir_path / 'data' / filename).read_text(encoding='utf8'))
 
-    def api_query(self, base_url: str, method: str, req: dict | None = None) -> dict:
+    def api_query(self, method: str, req: dict | None = None) -> dict:
         # Pretty ugly ... mock a kraken remote error
         if self.remote_errors:
             raise RemoteError('Kraken remote error')
 
         if self.use_original_kraken:
-            return super().api_query(base_url, method, req)
+            return super().api_query(method, req)
 
         if method == 'Balance':
             if self.random_balance_data:
